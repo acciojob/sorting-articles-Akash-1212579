00726f1dfe5afd74acb3bad71f426257af0ebd61
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-09-29 13:27:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function stripArticle(str) {
+  return str.replace(/^(a |an |the )/i, ""); 
+}
+
+bands.sort((a, b) => {
+  let strA = stripArticle(a).toLowerCase();
+  let strB = stripArticle(b).toLowerCase();
+  return strA.localeCompare(strB);
+});
+
+let ulItems = document.getElementById("band");
+bands.forEach(val=>{
+	let newli = document.createElement("li");
+	newli.textContent = val;
+	ulItems.appendChild(newli);
+});
